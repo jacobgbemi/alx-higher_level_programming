a9d181287e3d9599bf3e62a301ac9c331bec1702
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-10-03 12:34:19 +0000

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const texts = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const texts = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const words of texts) {
   console.log(words);
 }
