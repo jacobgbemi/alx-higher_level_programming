5b4db4d6b2a9b6d68776677f994e26028a41b66b
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-11-15 12:58:19 +0000

    fix task 4

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,11 +4,4 @@ const url = process.argv[2];
 const filename = process.argv[3];
 const fs = require('fs');
 const request = require('request');
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filename, body, 'utf8');
-  }
-});
+request(url).pipe(fs.createWriteStream(filename));
